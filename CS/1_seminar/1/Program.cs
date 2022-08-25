dbfd6bddf9dc68760f1e6b80a9e2e7431859de2e
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-25 17:28:36 +0300

    task 1

---
+++ CS/1_seminar/1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == b)
+{
+    Console.WriteLine("Оба числа одинаковы");
+}
+if(a > b)
+{
+    Console.WriteLine("Максимальное число: " + a);
+}
+if(a < b)
+{
+    Console.WriteLine("Максимальное число: " + b);
+}
