a8fa339ccc7554e596147972f037663227f20a7c
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-25 17:50:15 +0300

    task 2

---
+++ CS/1_seminar/2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if((num1 == num2) && (num2 == num3))
+{
+    Console.WriteLine("Все числа одинаковы");
+}
+if((num1 > num2) && (num1 > num3))
+{
+    Console.WriteLine("Максимальное число: " + num1);
+}
+if((num2 > num1) && (num2 > num3))
+{
+    Console.WriteLine("Максимальное число: " + num2);
+}
+if((num3 > num2) && (num3 > num1))
+{
+    Console.WriteLine("Максимальное число: " + num3);
+}
