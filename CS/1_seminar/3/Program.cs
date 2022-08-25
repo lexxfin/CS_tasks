e4746336bd3208670f24d68090ff1b908f34222a
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-25 17:59:36 +0300

    task 3

---
+++ CS/1_seminar/3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
