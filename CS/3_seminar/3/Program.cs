8374392a8d049953d6105c023a5212af36626a11
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-26 22:10:51 +0300

    s3e3

---
+++ CS/3_seminar/3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] n_cube = new int[n];
+for (int i = 1; i <= n; i++)
+{
+    n_cube[i - 1] = (int)Math.Pow(i, 3);
+    Console.Write("{0} ", n_cube[i - 1]);
+}
