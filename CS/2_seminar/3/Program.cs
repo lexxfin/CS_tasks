d978cea6f52a4e06a017bfb23753c0c5b9023842
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-26 23:09:51 +0300

    s2e3

---
+++ CS/2_seminar/3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру дня недели (например, 1 это понедельник) ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n == 6) || (n == 7))
+{
+    Console.WriteLine("да");
+}
+else if ((n >= 1) && (n <= 5))
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("Введено число не соответствующее дню недели");
+}
