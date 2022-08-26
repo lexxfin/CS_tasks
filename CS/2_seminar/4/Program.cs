3c5e93454ec256d0328a016c3d6e020423e76053
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-26 23:23:06 +0300

    s2e4

---
+++ CS/2_seminar/4/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = new Random().Next(1, 1000001);
+if (n % (7 * 23) == 0)
+{
+    Console.WriteLine($"Число {n} кратно 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine($"Число {n} не кратно 7 и 23 одновременно");
+}
