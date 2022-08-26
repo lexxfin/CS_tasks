41ddb5dc902570c248a4a042bf6116ed46229bef
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-26 20:46:43 +0300

    s3e2

---
+++ CS/3_seminar/2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите координаты первой точки через пробел (например: 3 -2 6): ");
+string[] nums_str = Console.ReadLine().Split();
+int[] a = new int[nums_str.Length];
+for (int i = 0; i < nums_str.Length; i++)
+{
+    a[i] = Convert.ToInt32(nums_str[i]);
+}
+Console.Write("Введите координаты второй точки через пробел (например: -3 2 0): ");
+nums_str = Console.ReadLine().Split();
+int[] b = new int[nums_str.Length];
+for (int i = 0; i < nums_str.Length; i++)
+{
+    b[i] = Convert.ToInt32(nums_str[i]);
+}
+Console.Write("Расстояние между точками равно: ");
+Console.WriteLine(Math.Sqrt(Math.Pow(b[0] - a[0], 2) + Math.Pow(b[1] - a[1], 2) + Math.Pow(b[2] - a[2], 2)));
