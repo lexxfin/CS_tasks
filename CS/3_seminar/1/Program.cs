ed18ec4a937944280632432a86bd233f28f01065
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-26 19:01:41 +0300

    s3e1

---
+++ CS/3_seminar/1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n < 10000) || (n > 99999))
+{
+    Console.WriteLine("Введенное число не пятизначное");
+}
+else
+{
+    if ((n / 10000 == n % 10) && ((n % 10000) / 1000 == (n % 100) / 10))
+    {
+        Console.WriteLine("Введенное число полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не полиндром");
+    }
+}
