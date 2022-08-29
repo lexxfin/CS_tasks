03d1eaf70bbf9dcda4dfa38b5d60759310f3ce81
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-08-30 02:28:36 +0300

    s4e2

---
+++ CS/4_seminar/2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int tmp = n;
+int sum = 0;
+while (tmp > 0 && tmp % 10 != 0)
+{
+    if (n % (tmp % 10) == 0)
+    {
+        sum += tmp % 10;
+    }
+    tmp /= 10;
+}
+Console.WriteLine($"{n} -> {sum}");
