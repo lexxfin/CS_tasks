b08feebec9415e1760661cd6c3a467b442457b36
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-09-04 17:07:05 +0300

    s5e1

---
+++ CS/5_seminar/1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите количество чисел в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+Random rnd = new Random();
+int count_of_even = 0;
+for (int i = 0; i < n; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+    if (array[i] % 2 == 0) count_of_even++;
+}
+Console.WriteLine("[" + String.Join(", ", array) + "] -> " + count_of_even.ToString());
