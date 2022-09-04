211915eb0a1b39987076b2c7fe94c66f442a601b
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-09-04 17:51:08 +0300

    s5e2

---
+++ CS/5_seminar/2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите количество чисел в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    array[i] = rnd.Next(100);
+    if (array[i] % 2 == 0 && i % 2 != 0) sum += array[i];
+}
+Console.WriteLine("[" + String.Join(", ", array) + "] -> " + sum.ToString());
