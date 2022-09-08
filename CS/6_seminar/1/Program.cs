fb2b0a9438d6f0f89813e69fcc17ac571f53a501
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-09-08 15:55:20 +0300

    s6e1

---
+++ CS/6_seminar/1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите количество чисел М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[M];
+int countOfPositive = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"Введите {i + 1}-е число: ");
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+    if (nums[i] > 0) countOfPositive++;
+}
+Console.WriteLine("[" + String.Join(", ", nums) + $"] -> {countOfPositive}");
