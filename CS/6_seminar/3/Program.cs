83b68d369e309e5d0e8cddb92dec19de39455338
Alexey Chernyshov <lexxfin@me.com>
2022-09-10 00:23:31 +0300

    s6e3

---
+++ CS/6_seminar/3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите количество чисел в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < n; i++) array[i] = rnd.Next(15);
+for (int i = 0; i < n / 2; i++) sum += array[i] * array[n - i - 1];
+Console.WriteLine($"[{String.Join(", ",array)}] -> {sum}");
