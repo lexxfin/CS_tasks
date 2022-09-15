6bd73d901db3f95caa816eca2d576bf0a81a68cf
Алексей Чернышов <lexxfin@lexxfins-MacBook-Pro-13.local>
2022-09-15 12:12:04 +0300

    s7e1

---
+++ CS/7_seminar/1/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = 3, n = 4;
+double[,] arr = new double[m, n];
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = (double)rnd.Next(-100, 100) / 10;
+        Console.Write($"{arr[i, j]}\t");
+    }
+    Console.WriteLine();
+}
