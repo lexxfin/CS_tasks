9c97af69af6242b0a77c22bd13e97ee6c4a14105
Alexey Chernyshov <lexxfin@me.com>
2022-09-15 22:50:35 +0300

    s8e1

---
+++ CS/8_seminar/1/Program.cs
@@ -0,0 +1,21 @@
+﻿int row = 3, col = 4, sum, minSum = 10000, rowMinSum = 0;
+int[,] arr = new int[row, col];
+Random rnd = new Random();
+Console.WriteLine("\n");
+for (int i = 0; i < row; i++)
+{
+    sum = 0;
+    for (int j = 0; j < col; j++)
+    {
+        arr[i, j] = rnd.Next(10);
+        Console.Write($"{arr[i, j]}\t");
+        sum += arr[i, j];
+    }
+    Console.WriteLine("\n");
+    if (sum < minSum)
+    {
+        rowMinSum = i + 1;
+        minSum = sum;
+    }
+}
+Console.WriteLine($"{rowMinSum} строка имеет минимальную сумму элементов");
