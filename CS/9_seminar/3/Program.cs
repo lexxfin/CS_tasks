ac7c65683fb8eeb459585a59190f00fc7f513083
Alexey Chernyshov <lexxfin@me.com>
2022-09-22 10:30:37 +0300

    s9e3

---
+++ CS/9_seminar/3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Возвращает -1 если m или n меньше нуля
+Int128 ackermanFunc(Int128 m, Int128 n)
+{
+    if (m == 0 && n > 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return ackermanFunc(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return ackermanFunc(m - 1, ackermanFunc(m, n - 1));
+    }
+    else return -1;
+}
+
+Int128 m = 3, n = 7;
+Console.WriteLine($"Для m = {m} и n = {n} Функция Аккермана A(m, n) = {ackermanFunc(m, n)}");
